837f533b0ebc881fe4ada774d15383dda3d6468a
USUARIO <USUARIO@EQUIPO>
2022-06-08 23:35:55 -0500

    Modificacion Carga Inicial Hola Mundo Nuevo

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo Nuevo");
         
         // TODO code application logic here
     }
